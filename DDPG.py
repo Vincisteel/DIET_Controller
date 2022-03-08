65b36ad06b97440369ec96552222e0d97bfffa87
Arnab Chatterjee <43605560+Vincisteel@users.noreply.github.com>
2022-03-08 16:11:11 +0100

    Add files via upload

--- DDPG.py
+++ DDPG.py
@@ -1,15 +1,17 @@
 # DDPG Thermal Control Policy Implementation
 
 # Importing the libraries
-import os
-import time
 import random
 import torch
 import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
 import math
-import subprocess  # to run the TRNSYS simulation
+from pyfmi import load_fmu
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.offline as pyo
+import os
 import pandas as pd
 
 
@@ -101,7 +103,7 @@ class DDPG(object):
 
         # Learning Process for the DDPG Algorithm
 
-    def train(self, replay_buffer, iterations, batch_size=100, discount=0.99, tau=0.005, policy_noise=0.2,
+    def train(self, replay_buffer, iterations, batch_size=128, discount=0.99, tau=0.05, policy_noise=0.2,
               noise_clip=0.5):
 
         for it in range(iterations):
@@ -168,70 +170,26 @@ class DDPG(object):
         self.critic.load_state_dict(torch.load('%s/%s_critic.pth' % (directory, filename)))
 
 
-# We make a function that evaluates the policy by calculating its average reward over 3 episodes
-def evaluate_policy(eval_episodes=1):
-    avg_reward = 0.
-    for eval_sim in range(eval_episodes):
-        # We reset the state of the environment [Tair, RHair, Tmrt, Vair, Tout, Clo, Met, Occ, Qheat, HOY]
-        obs = np.array([20.0, 50.0, 20.0, 0.1, 5.5, 1.0, 1.0, 0.0001, 0.0001, 0.0])
-
-        # Store the first observations in the text file
-        state_txt = open(r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\py_state.dat", "w")
-        state_txt.truncate(0)
-        state_txt.write('\t' + str(obs[0]) + '\t' + str(obs[1]) + '\t' + str(obs[2]) + '\t' + str(obs[3]) + '\t' +
-                        str(obs[4]) + '\t' + str(obs[5]) + '\t' + str(obs[6]) + '\t' + str(obs[7]) + '\t' +
-                        str(obs[8]) + '\n')
-        state_txt.close()
-
-        # Erase the data from the previous episodes
-        next_state_txt = open(r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\py_next_state.dat", "w")
-        next_state_txt.truncate(0)
-        next_state_txt.close()
-
-        action_txt = open(r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\py_action.dat", "w")
-        action_txt.truncate(0)
-        action_txt.close()
-
-        reward_txt = open(r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\py_reward.dat", "w")
-        reward_txt.truncate(0)
-        reward_txt.close()
-
-        pmv_txt = open(r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\py_pmv.dat", "w")
-        pmv_txt.truncate(0)
-        pmv_txt.close()
-
-        # Running TRNSYS simulation
-        subprocess.run([r"C:\TRNSYS18\Exe\TrnEXE64.exe",
-                        r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\BuildingModel_1day.dck"])
-
-        # Reading the reward from text file and calculating the episode reward
-        reward_data = pd.read_csv(
-            r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\Backup\7\Training\py_reward.dat",
-            sep="\s+", usecols=[0], names=[0], skiprows=2)
-        avg_reward += reward_data[0].sum
-
-    avg_reward /= eval_episodes
-    print("---------------------------------------")
-    print("Average Reward over the Evaluation Step: %f" % avg_reward)
-    print("---------------------------------------")
-    return avg_reward
-
-
 # We set the parameters
-start_sim = 2  # Number of simulations after which the reinforcement learning defines the actions
-max_sim = 10  # Total number of Trnsys simulations for training
-sim_num = 0
 save_models = True  # Boolean checker whether or not to save the pre-trained model
 expl_noise = 0.1  # Exploration noise - STD value of exploration Gaussian noise
-batch_size = 100  # Size of the batch
+batch_size = 128  # Size of the batch
 discount = 0.99  # Discount factor gamma, used in the calculation of the total discounted reward
-tau = 0.005  # Target network update rate
+tau = 0.05  # Target network update rate
 policy_noise = 0.2  # STD of Gaussian noise added to the actions for the exploration purposes
 noise_clip = 0.5  # Maximum value of the Gaussian noise added to the actions (policy)
-comfort_lim = 0.5  # Limiting value of comfort(+/-) on the PMV index
-alpha = 0.5  # Adjusting co-efficient for the comfort reward
+alpha = 3  # Adjusting co-efficient for the comfort reward
 beta = 1  # Adjusting co-efficient for the energy reward
-obs = np.array([20.0, 50.0, 20.0, 0.1, 5.5, 1.0, 1.0, 0.0001, 0.0001])  # Initial state of the trnsys env
+modelname = 'CELLS_v1'
+days = 151  # Number of days the simulation is run for
+hours = 24  # Number of hours each day the simulation is run for
+minutes = 60
+seconds = 60
+ep_timestep = 6  # Number of timesteps per hour
+numsteps = days * hours * ep_timestep
+timestop = days * hours * minutes * seconds
+secondstep = timestop / numsteps
+comfort_lim = 0
 
 # We create a filename for the two saved models: the Actor and Critic Models
 file_name = "%s_%s" % ("DDPG", "DIET")
@@ -240,7 +198,7 @@ print("Settings: %s" % file_name)
 print("---------------------------------------")
 
 # We get the necessary information on the states and actions in the chosen environment
-state_dim = 9
+state_dim = 6
 action_dim = 1
 max_action = 21
 
@@ -250,12 +208,9 @@ policy = DDPG(state_dim, action_dim, max_action)
 # We create the Experience Replay memory
 replay_buffer = ReplayBuffer()
 
-# We define a list where all the evaluation results over 3 episodes are stored
-evaluations = []
-
 
 # Function taken from CBE comfort tool to calculate the pmv value for comfort evaluation
-def comfPMV(ta, tr, vel, rh, met, clo, wme=0):
+def comfPMV(ta, tr, rh, vel=0.1, met=1.1, clo=1, wme=0):
     pa = rh * 10 * math.exp(16.6536 - 4030.183 / (ta + 235))
 
     icl = 0.155 * clo  # thermal insulation of the clothing in M2K/W
@@ -323,121 +278,125 @@ def comfPMV(ta, tr, vel, rh, met, clo, wme=0):
     return pmv
 
 
-def trnsys_sim(tair_in, rh_in, tmrt_in, vair_in, tout_in, clo_in, met_in, occ_in, qheat_in):
-    global obs
-    # Retrieve values from the parameters and inputs from Trnsys
+os.chdir(r'C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\Cosimulation_energyplus')
 
-    # Calculate the values of the new actions
-    if sim_num < start_sim:
-        action = round(random.uniform(16, 21), 1)  # Choosing random values between 16 and 24 deg
-    else:  # After 2 episodes, we switch to the model
-        action_arr = policy.select_action(obs)
-        # If the explore_noise parameter is not 0, we add noise to the action and we clip it
-        if expl_noise != 0:
-            action_arr = (action_arr + np.random.normal(0, expl_noise, size=1)).clip(16.0, 21.0)
-            action = action_arr[0]
+for sim_num in range(15):
 
-    # The agent performs the action in the environment, then reaches the next state and receives the reward
-    new_obs = np.array([tair_in, rh_in, tmrt_in, vair_in, tout_in, clo_in, met_in, occ_in, qheat_in])
+    model = load_fmu(modelname + '.fmu')
+    opts = model.simulate_options()  # Get the default options
+    opts['ncp'] = numsteps  # Specifies the number of timesteps
+    opts['initialize'] = False
+    simtime = 0
+    model.initialize(simtime, timestop)
+    index = 0
+    t = np.linspace(0.0, numsteps-1, numsteps)
+    inputcheck_heating = np.zeros((numsteps, 1))
+    tair = np.zeros((numsteps, 1))
+    rh = np.zeros((numsteps, 1))
+    tmrt = np.zeros((numsteps, 1))
+    tout = np.zeros((numsteps, 1))
+    occ = np.zeros((numsteps, 1))
+    qheat = np.zeros((numsteps, 1))
+    pmv = np.zeros((numsteps, 1))
+    reward = np.zeros((numsteps, 1))
+    action = np.zeros((numsteps, 1))
+    state = np.zeros((numsteps, 6))
 
-    pmv = comfPMV(tair_in, tmrt_in, vair_in, rh_in, met_in, clo_in)
+    if sim_num != 0:
+        print("Total timesteps: {} Episode Num: {} Reward: {}".format(numsteps, sim_num, np.sum(reward.flatten())))
+        policy.train(replay_buffer, (numsteps - 1) * sim_num, batch_size, discount, tau, policy_noise, noise_clip)
 
-    reward = beta * (1 - (qheat_in/15000)) + alpha * (1 - ((pmv + 0.5) ** 2)) * occ_in
+        if sim_num > 1:
+            policy.save(file_name, directory="./pytorch_models/01032022")  # Change the folder name here
 
-    obs = new_obs
+    while simtime < timestop:
 
-    # Step 6: Return the new values based on which action will be performed in Trnsys
-    return action, reward, pmv
+        if sim_num < 6:
+            action[index] = round(random.uniform(12, 30), 1)  # Choosing random values between 12 and 30 deg
 
+        else:  # After 1 episode, we switch to the model
+            action_arr = policy.select_action(obs)
+            # If the explore_noise parameter is not 0, we add noise to the action and we clip it
+            if expl_noise != 0:
+                action_arr = (action_arr + np.random.normal(0, expl_noise, size=1)).clip(12.0, 30.0)
+                action[index] = action_arr[0]
 
-def run_training():
-    global sim_num
-    action_data = pd.DataFrame()
-    reward_data = pd.DataFrame()
+        model.set('Thsetpoint_diet', action[index])
+        res = model.do_step(current_t=simtime, step_size=secondstep, new_step=True)
+        inputcheck_heating[index] = model.get('Thsetpoint_diet')
+        tair[index], rh[index], tmrt[index], tout[index], qheat[index], occ[index], inputcheck_heating[index] = model.get(['Tair', 'RH', 'Tmrt', 'Tout', 'Qheat', 'Occ', 'Thsetpoint_diet'])
+        state[index][0], state[index][1], state[index][2], state[index][3], state[index][4], state[index][5] = tair[index], rh[index], tmrt[index], tout[index], occ[index], qheat[index]
+        pmv[index] = comfPMV(tair[index], tmrt[index], rh[index])
+        reward[index] = beta * (1 - (qheat[index]/(800*1000))) + alpha * (1 - abs(pmv[index] + 0.5)) * occ[index]   # * int(bool(occ[index]))
 
-    for ep_num in range(max_sim):
+        if index == 0:
+            obs = np.array([tair[index], rh[index], tmrt[index], tout[index], occ[index], qheat[index]]).flatten()
 
-        sim_num = ep_num
-        # If we are not at the very beginning, we start the training process of the model
-        if ep_num != 0:
-            print("Total simulations: {} Episode Num: {} Reward: {}".format(action_data.size, ep_num + 1,
-                                                                            reward_data[0].sum))
-            policy.train(replay_buffer, action_data.size, batch_size, discount, tau, policy_noise, noise_clip)
+        else:
+            new_obs = np.array([tair[index], rh[index], tmrt[index], tout[index], occ[index], qheat[index]]).flatten()
+            # We store the new transition into the Experience Replay memory (ReplayBuffer)
+            replay_buffer.add((obs, new_obs, action[index], reward[index]))
+            obs = new_obs
 
-            evaluations.append(evaluate_policy())
-            policy.save(file_name, directory="./pytorch_models/latest")
-            np.save("./results/%s" % file_name, evaluations)
+        simtime += secondstep
+        index += 1
 
-        # We reset the state of the environment [Tair, RHair, Tmrt, Vair, Tout, Clo, Met, Occ, Qheat, HOY]
-        obs = np.array([20.0, 50.0, 20.0, 0.1, 5.5, 1.0, 1.0, 0.0001, 0.0001])
+    # Writing to .csv files to save the data from the episode
+    np.savetxt("./Training_Data/01032022/Ep"+str(sim_num+1)+"/state.csv", state[:-1, :], delimiter=",")
+    np.savetxt("./Training_Data/01032022/Ep"+str(sim_num+1)+"/next_state.csv", state[1:, :], delimiter=",")
+    np.savetxt("./Training_Data/01032022/Ep"+str(sim_num+1)+"/action.csv", action[1:, :], delimiter=",")
+    np.savetxt("./Training_Data/01032022/Ep"+str(sim_num+1)+"/reward.csv", reward[1:, :], delimiter=",")
+    np.savetxt("./Training_Data/01032022/Ep"+str(sim_num+1)+"/pmv.csv", pmv[1:, :], delimiter=",")
 
-        # Store the first observations in the text file
-        state_txt = open(r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\py_state.dat", "w")
-        state_txt.truncate(0)
-        state_txt.write('\t' + str(obs[0]) + '\t' + str(obs[1]) + '\t' + str(obs[2]) + '\t' + str(obs[3]) + '\t' +
-                        str(obs[4]) + '\t' + str(obs[5]) + '\t' + str(obs[6]) + '\t' + str(obs[7]) + '\t' +
-                        str(obs[8]) + '\n')
-        state_txt.close()
+    # Plotting the summary of simulation
+    fig = make_subplots(rows=6, cols=1, shared_xaxes=True, vertical_spacing=0.04,
+                        specs=[[{"secondary_y": False}], [{"secondary_y": False}], [{"secondary_y": False}],
+                               [{"secondary_y": True}], [{"secondary_y": True}], [{"secondary_y": False}]])
 
-        # Erase the data from the previous episodes
-        next_state_txt = open(r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\py_next_state.dat", "w")
-        next_state_txt.truncate(0)
-        next_state_txt.close()
+    # Add traces
+    fig.add_trace(go.Scatter(name='Tair(state)', x=t, y=tair.flatten(), mode='lines', line=dict(width=1, color='cyan')),
+                  row=1, col=1)
+    fig.add_trace(go.Scatter(name='Tair_avg', x=t, y=pd.Series(tair.flatten()).rolling(window=24).mean(), mode='lines',
+                  line=dict(width=2, color='blue')), row=1, col=1)
+    fig.add_trace(go.Scatter(name='Tset(action)', x=t, y=action.flatten(), mode='lines', line=dict(width=1, color='fuchsia')),
+                  row=2, col=1)
+    fig.add_trace(go.Scatter(name='Tset_avg', x=t, y=pd.Series(action.flatten()).rolling(window=24).mean(), mode='lines',
+                  line=dict(width=2, color='purple')), row=2, col=1)
+    fig.add_trace(go.Scatter(name='Pmv', x=t, y=pmv.flatten(), mode='lines', line=dict(width=1, color='gold')),
+                  row=3, col=1)
+    fig.add_trace(go.Scatter(name='Pmv_avg', x=t, y=pd.Series(pmv.flatten()).rolling(window=24).mean(), mode='lines',
+                  line=dict(width=2, color='darkorange')), row=3, col=1)
+    fig.add_trace(go.Scatter(name='Heating', x=t, y=qheat.flatten(), mode='lines', line=dict(width=1, color='red')),
+                  row=4, col=1, secondary_y=False)
+    fig.add_trace(go.Scatter(name='Heating_cumulative', x=t, y=np.cumsum(qheat.flatten()), mode='lines',
+                  line=dict(width=2, color='darkred')), row=4, col=1, secondary_y=True)
+    fig.add_trace(go.Scatter(name='Reward', x=t, y=reward.flatten(), mode='lines', line=dict(width=1, color='lime')),
+                  row=5, col=1, secondary_y=False)
+    fig.add_trace(go.Scatter(name='Reward_cum', x=t, y=np.cumsum(reward.flatten()), mode='lines',
+                  line=dict(width=2, color='darkgreen')), row=5, col=1, secondary_y=True)
+    fig.add_trace(go.Scatter(name='Occupancy', x=t, y=occ.flatten(), mode='lines',
+                  line=dict(width=1, color='black')), row=6, col=1)
 
-        action_txt = open(r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\py_action.dat", "w")
-        action_txt.truncate(0)
-        action_txt.close()
+    # Set x-axis title
+    fig.update_xaxes(title_text="Timestep (-)", row=6, col=1)
 
-        reward_txt = open(r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\py_reward.dat", "w")
-        reward_txt.truncate(0)
-        reward_txt.close()
+    # Set y-axes titles
+    fig.update_yaxes(title_text="<b>Tair</b> (°C)", range=[10, 24], row=1, col=1)
+    fig.update_yaxes(title_text="<b>Tset</b> (°C)", range=[12, 30], row=2, col=1)
+    fig.update_yaxes(title_text="<b>PMV</b> (-)", row=3, col=1)
+    fig.update_yaxes(title_text="<b>Heat Power</b> (kJ/hr)", row=4, col=1, secondary_y=False)
+    fig.update_yaxes(title_text="<b>Heat Energy</b> (kJ)", row=4, col=1, secondary_y=True)
+    fig.update_yaxes(title_text="<b>Reward</b> (-)", row=5, col=1, range=[-5, 5], secondary_y=False)
+    fig.update_yaxes(title_text="<b>Tot Reward</b> (-)", row=5, col=1, secondary_y=True)
+    fig.update_yaxes(title_text="<b>Fraction</b> (-)", row=6, col=1)
 
-        pmv_txt = open(r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\py_pmv.dat", "w")
-        pmv_txt.truncate(0)
-        pmv_txt.close()
+    fig.update_xaxes(nticks=50)
 
-        # Running TRNSYS simulation
-        subprocess.run([r"C:\TRNSYS18\Exe\TrnEXE64.exe",
-                        r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\BuildingModel_2day.dck"])
+    fig.update_layout(template='plotly_white', font=dict(family="Courier New, monospace", size=12),
+                      legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=1))
 
-        # Reading from the text files to fill the replay buffer
-        state_data = pd.read_csv(
-            r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\Backup\7\Training\py_state.dat",
-            sep="\s+", usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8], names=[0, 1, 2, 3, 4, 5, 6, 7, 8],
-            skiprows=2, skipfooter=1)
-        next_state_data = pd.read_csv(
-            r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\Backup\7\Training\py_next_state.dat", sep="\s+",
-            usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8], names=[0, 1, 2, 3, 4, 5, 6, 7, 8], skiprows=2)
-        action_data = pd.read_csv(
-            r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\Backup\7\Training\py_action.dat",
-            sep="\s+", usecols=[0], names=[0], skiprows=2)
-        reward_data = pd.read_csv(
-            r"C:\Users\achatter\Desktop\PhDResearch\DIET\Controller\Backup\7\Training\py_reward.dat",
-            sep="\s+", usecols=[0], names=[0], skiprows=2)
+    pyo.plot(fig, filename="./Training_Data/01032022/Ep"+str(sim_num+1)+"/results.html")
 
-        for ind in range(action_data.size):
-            obs = np.array(
-                [state_data[0][ind], state_data[1][ind], state_data[2][ind], state_data[3][ind], state_data[4][ind],
-                 state_data[5][ind], state_data[6][ind], state_data[7][ind],
-                 state_data[8][ind]])
+    del model, opts
 
-            new_obs = np.array(
-                [next_state_data[0][ind], next_state_data[1][ind], next_state_data[2][ind], next_state_data[3][ind],
-                 next_state_data[4][ind], next_state_data[5][ind],
-                 next_state_data[6][ind], next_state_data[7][ind], next_state_data[8][ind]])
-
-            action = np.array([action_data[0][ind]])
-            reward_arr = np.array([reward_data[0][ind]])
-
-            # Step 5: We store the new transition into the Experience Replay memory (ReplayBuffer)
-            replay_buffer.add((obs, new_obs, action, reward_arr))
-
-    # We add the last policy evaluation to our list of evaluations and we save our model
-    evaluations.append(evaluate_policy())
-    if save_models:
-        policy.save("%s" % file_name, directory="./pytorch_models/latest")
-    np.save("./results/%s" % file_name, evaluations)
-
-
-
-
+policy.save(file_name, directory="./pytorch_models/01032022")  # Change the folder name here
